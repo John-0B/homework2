a027418af49cbbec96a2470a5974efe4fc2e4d27
John-0B <ivbg@yandex.ru>
2024-07-04 23:48:29 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+total_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+print('Course:', course_name, ',', 'Total tasks:', total_tasks, ',', 'Hours spent:', hours_spent, ',', 'Average execution time:', hours_spent / total_tasks)
